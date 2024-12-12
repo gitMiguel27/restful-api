a4267f2cebaae353644708aacb62af9c66566bce
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-12-12 10:06:03 -0500

    added a post route

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 3000;
 const users = require('./data/users');
 const posts = require('./data/posts');
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('Work in progress...')
 });
@@ -12,6 +17,25 @@ app.get('/', (req, res) => {
 // get all users
 app.get('/api/users', (req, res) => {
     res.json(users);
+});
+
+app.post('/api/users', (req, res) => {
+    if(req.body.name && req.body.username && req.body.email) {
+        if(users.find((u) => u.username == req.body.username)) {
+            res.json({ error: 'Username Already Taken' });
+            return;
+        };
+
+        const user = {
+            id: users[user.length - 1].id + 1,
+            name: req.body.name,
+            username: req.body.username,
+            email: req.body.email
+        };
+    
+        users.push(user);
+        res.json(users[users.length - 1]);
+    } else res.json({ error: 'Insufficient Data' });
 });
 
 app.get('/api/users/:id', (req, res, next) => {
@@ -33,7 +57,7 @@ app.get('/api/posts/:id', (req, res, next) => {
 
 app.use((req, res) => {
     res.status(404)
-    res.json({error: 'resource not found'});
+    res.json({ error: 'resource not found' });
 });
 
 app.listen(port, () => {
